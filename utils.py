b14b1b0370ecd223e293aea81cad96246a867e8d
amaneth <amankassahun190@gmail.com>
2022-05-07 12:41:45 +0000

    change indexing the labels

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@
 def train_test_split(data, train_size):
     size = len(data)
     train_size = int(train_size * size)
-    X_train, y_train = data['review'][:train_size], data['sentiment'][:train_size]
-    X_test, y_test = data['review'][train_size:], data['sentiment'][train_size:]
-    cats = data['sentiment'].unique().tolist()
+    X_train, y_train = data.iloc[:,0][:train_size], data.iloc[:,1][:train_size]
+    X_test, y_test = data.iloc[:,0][train_size:], data.iloc[:,1][train_size:]
+    cats = data.iloc[:,1].unique().tolist()
     y_test = y_test.apply(lambda x : cats.index(x))
     return X_train, y_train, X_test, y_test
