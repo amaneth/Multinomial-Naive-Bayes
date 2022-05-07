6c7ad541c6472f6fe41486cee8ce2d0c44f2ac30
amaneth <amankassahun190@gmail.com>
2022-05-07 12:01:31 +0000

    initial commit

---
+++ predict.py
@@ -0,0 +1,24 @@
+import numpy as np
+import argparse
+import pickle
+
+def test(model,text):
+    #returns the prediction of categories for a test document
+    X_new= model.transform(text)
+    log_probablity= X_new[..., np.newaxis]*np.log(model.likelihood[np.newaxis,...])
+    cat_log_probablity= log_probablity.sum(axis=1)
+
+    category_probablity = np.log(model.prior) + cat_log_probablity
+    prediction=np.argmax(category_probablity,axis=1)
+    print("The prediction:", model.labels[prediction[0]]) 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--text', help='the text to be predicted', required=True, type=str)
+arguments= vars(parser.parse_args())
+text=arguments['text']
+with open('./model/model.pkl', 'rb') as handle:
+    model=pickle.load(handle)
+
+test(model, [text])
+
+
