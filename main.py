c7d1379aa2567487b05d3a8937c0348d5399c4d3
amaneth <amankassahun190@gmail.com>
2022-05-07 11:57:33 +0000

    save the model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config import args
 import argparse
 import pandas as pd
 import nltk
+import pickle
 
 from utils import train_test_split
 from model import MultinomialNB
@@ -17,18 +18,23 @@ train_size= args.train_size
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-k', '--k', help='k is the laplacian smooting constant', default=k, type=float)
-parser.add_argument('-p', '--dataset', help='path to the dataset', default=metadata, type=str)
-parser.add_argument('-t', '--trainsize', help='the train data size', default=train_size, type=float)
+parser.add_argument('--k', help='k is the laplacian smooting constant', default=k, type=float)
+parser.add_argument('--dataset', help='path to the dataset', default=metadata, type=str)
+parser.add_argument('--trainsize', help='the train data size', default=train_size, type=float)
+parser.add_argument('--savemodel', help='save the model as file', default=True, type=bool)
 arguments = vars(parser.parse_args())
 k= arguments['k']
 path= arguments['dataset']
 train_size= arguments['trainsize']
+save_model= arguments['savemodel']
 
-data= pd.read_csv(path)[:1000] 
+data= pd.read_csv(path) 
 X_train, y_train, X_test, y_test= train_test_split(data, train_size)
 naive= MultinomialNB(k)
 fit(naive, X_train, y_train)
 print("Training is done.")
+if save_model:
+    with open('./model/model.pkl','wb') as handle:
+        pickle.dump(naive, handle, protocol=pickle.HIGHEST_PROTOCOL)
 test(naive, X_test, y_test)
 
