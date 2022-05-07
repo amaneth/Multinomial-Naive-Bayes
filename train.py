1d911727f3f473ff1ee982973e9eeb846c275bf8
amaneth <amankassahun190@gmail.com>
2022-05-07 12:01:00 +0000

    change classes to labels

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ import numpy as np
 def fit(model, X, y):
     #fit the the train data to the model
     documents = model.tokenize(X, train=True)
-    classes=y.unique()
-    num_classes=len(classes)
-    labels = y.map(lambda x: np.where(classes==x)[0][0])
+    model.labels=y.unique()
+    num_classes=len(model.labels)
+    labels = y.map(lambda x: np.where(model.labels==x)[0][0])
 
     counts = model.count_words(documents, labels)
     model.prior_probablity(labels)
